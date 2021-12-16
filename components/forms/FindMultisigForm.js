a1cc7b7bd8ba70d1bf0e4381eab6fbf45fbb0b9b
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2021-12-16 12:09:53 +0700

    add template for handleConnect

--- components/forms/FindMultisigForm.js
+++ components/forms/FindMultisigForm.js
@@ -13,44 +13,24 @@ class FindMultisigForm extends React.Component {
     this.state = {
       address: "",
       keyError: "",
-      processing: false,
     };
   }
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  handleSearch = async () => {
-    this.setState({ processing: true });
+  handleConnect(){
+    // connect to wallet here
+    this.setState({address : ""})
 
     this.props.router.push(`/multi/${this.state.address}`);
-  };
+  }
 
   render() {
     return (
       <StackableContainer>
-        <StackableContainer lessPadding>
-          <p>
-            Enter your 0x address below
-          </p>
-        </StackableContainer>
-        <StackableContainer>
-          <Input
-            onChange={this.handleChange}
-            value={this.state.address}
-            label="Ox Address"
-            name="address"
-            placeholder="0xffe7aba28065e451c71f4bb317c564bf9f03e1d2"
-          />
-          <Button
-            label="Use this address"
-            onClick={this.handleSearch}
-            primary
-          />
-        </StackableContainer>
+        <Button
+          label="Connect to your Metamask"
+          onClick={this.handleSearch}
+          primary
+        />
         <style jsx>{`
           .multisig-form {
             display: flex;
